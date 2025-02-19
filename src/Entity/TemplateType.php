ef1702a73b26992314cc04b409d2ec4dcdb6bd71
Stephane Rathgeber <stephane@kiora.tech>
2025-02-19 18:04:09 +0100

    upload template

---
+++ src/Entity/TemplateType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Entity;
+
+use Symfony\Contracts\Translation\TranslatableInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+enum TemplateType: string implements TranslatableInterface
+{
+
+    public const array MIME_TYPE_MAPPING = [
+        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' => self::EXCEL,
+        'application/vnd.ms-excel' => self::EXCEL,
+        'application/msword' => self::DOCUMENT,
+        'application/vnd.openxmlformats-officedocument.wordprocessingml.document' => self::DOCUMENT,
+        'application/rtf' => self::DOCUMENT,
+    ];
+
+    case EXCEL = 'excel';
+    case DOCUMENT = 'document';
+
+    public function trans(TranslatorInterface $translator, ?string $locale = null): string
+    {
+        return $translator->trans('template.type.' . $this->value, locale: $locale);
+    }
+
+    public static function values(): array
+    {
+        return array_column(self::cases(), 'value');
+    }
+
+    public static function fromMimeType(string $mimeType): self
+    {
+        if (!isset(self::MIME_TYPE_MAPPING[$mimeType])) {
+            throw new \InvalidArgumentException('Unsupported file type: ' . $mimeType);
+        }
+
+        return self::MIME_TYPE_MAPPING[$mimeType];
+    }
+
+}
