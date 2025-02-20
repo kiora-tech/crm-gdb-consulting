58727b9fe1d66c9b67d106472538b38f0e3444d3
Stephane Rathgeber <stephane@kiora.tech>
2025-02-20 22:12:53 +0100

    strat template

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ php bin/console doctrine:migrations:migrate
 
 # Fixer les permissions pour le dossier d'uploads
 chmod -R 775 /var/www/website/public/uploads
+chmod -R 777 /var/www/website/public/templates
 chmod -R 777 /var/www/website/var
-
+chown -R www-data:www-data /var/www/website
 php-fpm
